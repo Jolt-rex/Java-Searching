e8d7c4818d27ba07acb72542736ce8bba2b03c8c
Jolt-rex <leepotter010@gmail.com>
2023-05-08 12:02:19 +1000

    Binary search with recursion implemented

--- src/Search.java
+++ src/Search.java
@@ -1,8 +1,8 @@
 public class Search {
 
-    public int linearSearch(int[] items, int value) {
+    public int linearSearch(int[] items, int target) {
         for(int i = 0; i < items.length; i++)
-            if(items[i] == value)
+            if(items[i] == target)
                 return i;
 
         return -1;
@@ -12,17 +12,34 @@ public class Search {
         return binarySearchRecursion(items, target, 0, items.length - 1);
     }
 
-    private int binarySearchRecursion(int[] items, int target, int start, int end) {
-        int middle = (start + end) / 2;
+    private int binarySearchRecursion(int[] items, int target, int left, int right) {
+        int middle = (left + right) / 2;
         if(items[middle] == target)
             return middle;
 
-        if(start >= end)
+        if(left >= right)
             return -1;
 
         if(target < items[middle])
-            return binarySearchRecursion(items, target, start, middle - 1);
+            return binarySearchRecursion(items, target, left, middle - 1);
 
-        return binarySearchRecursion(items, target, middle + 1, end);
+        return binarySearchRecursion(items, target, middle + 1, right);
+    }
+
+    public int binarySearchIterative(int[] items, int target) {
+        int middle, left = 0, right = items.length - 1;
+
+        while(left <= right) {
+            middle = (left + right) / 2;
+            if(items[middle] == target)
+                return middle;
+
+            if(target < items[middle])
+                right = middle -1;
+            else
+                left = middle + 1;
+        }
+
+        return -1;
     }
 }
