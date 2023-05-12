84c6161ad2796a0474cef3c26e0d44000b08416a
Jolt-rex <leepotter010@gmail.com>
2023-05-12 21:25:09 +1000

    Ternary search implemented

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int[] items = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int[] items = { 9, 18, 24, 25 };
 
         System.out.println("Start search");
 
         var searcher = new Search();
-        System.out.println(searcher.binarySearchIterative(items, 9));
+        System.out.println(searcher.ternarySearch(items, 2));
     }
 }
